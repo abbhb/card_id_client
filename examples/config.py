7551ddd827ac0f515bcb92059fb476fa404669da
qc200 <qc2003020403@163.com>
2024-12-22 01:47:49 +0800

    feat: support video

--- examples/config.py
+++ examples/config.py
@@ -1,11 +1,12 @@
-search_uri = "/home/ai/card_id_client/examples/search.html"
-sidebar_uri = "/home/ai/card_id_client/examples/sidebar.html"
+search_uri = "G:/pycode/card_id_signin/examples/search.html"
+sidebar_uri = "/G:/pycode/card_id_signin/examples/sidebar.html"
 # 相对路径假如运行用的python example/find_face_qt_v2.py就用的根目录的数据库 直接进example里python find_face_qt_v2.py就是这个目录，所以写死
-db_uri = "/home/ai/card_id_client/examples/face_data.db"
+db_uri = "G:/pycode/card_id_signin/examples/face_data.db"
 card_mode = 16 # 打卡器输出模式：16为16进制输出，10为十进制输出
 server_port = 25401
 secert = "sbgh34y234"
 my_ip = "10.15.246.66"
+card_com = 'COM4'
 service_id = "ger354as24"
 consul_server_ip = "192.168.12.12"
 consul_server_port = 8500
